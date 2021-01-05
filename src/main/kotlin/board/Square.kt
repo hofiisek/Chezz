d56f32de6f282b7bae3277b058948cb7a3d19dc2
hofiisek <hofiisek@gmail.com>
2021-01-05 22:53:46 +0100

    Added build folder to gitignore

--- src/main/kotlin/board/Square.kt
+++ src/main/kotlin/board/Square.kt
@@ -3,6 +3,8 @@ package board
 import piece.Piece
 
 /**
+ * A single tile (or field)
+ *
  * @author Dominik Hoftych
  */
 data class Tile(val row: Int, val col: Int, var piece: Piece? = null) {
@@ -10,6 +12,10 @@ data class Tile(val row: Int, val col: Int, var piece: Piece? = null) {
     val rank: Int = 8 - row
 
     val file: Char = charArrayOf('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h')[col]
+
+    val color: Char = if (1 > (row + col) % 2) 'W' else 'B'
+
+    val text: String = "${file}${rank}}"
 
     init {
         require(row in 0..7 && col in 0..7) {
