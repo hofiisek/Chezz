26b1bca372eba9c4218ebfb5edbc2a8d203d7f22
hofiisek <hofiisek@gmail.com>
2021-02-02 21:33:43 +0100

    Moves rank and file properties from Square to Position

--- src/main/kotlin/board/Position.kt
+++ src/main/kotlin/board/Position.kt
@@ -1,5 +1,7 @@
 package board
 
+import kotlin.math.abs
+
 /**
  * Specific position on the chess board.
  *
@@ -11,6 +13,20 @@ package board
 data class Position(val row: Int, val col: Int) {
 
     /**
+     * Rank (row) of this position in range from 1 to 8
+     */
+    val rank: Int by lazy {
+        8 - row
+    }
+
+    /**
+     * File (column) of this position, named from 'a' to 'h'
+     */
+    val file: Char by lazy {
+        charArrayOf('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h')[col]
+    }
+
+    /**
      * Whether the position is on the board
      */
     val onBoard: Boolean = row in (0..7) && col in (0..7)
@@ -19,3 +35,7 @@ data class Position(val row: Int, val col: Int) {
 infix fun Position.add(other: Position) = Position(this.row + other.row, this.col + other.col)
 infix fun Position.add(other: Pair<Int, Int>) = this add Position(other.first, other.second)
 infix fun Position.add(square: Square) = this add square.position
+
+fun Position.add(x: Int, y: Int) = this add Position(x, y)
+
+infix fun Position.diffCols(other: Position): Int = abs(this.col - other.col)
