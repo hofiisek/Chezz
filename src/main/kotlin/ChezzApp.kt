41a6f33cde402817d2850e37107519e730ad1bfd
hofiisek <hofiisek@gmail.com>
2021-02-11 23:06:10 +0100

    Added icon showing in the app window

--- src/main/kotlin/ChezzApp.kt
+++ src/main/kotlin/ChezzApp.kt
@@ -1,3 +1,4 @@
+import javafx.scene.image.Image
 import javafx.stage.Stage
 import tornadofx.App
 import tornadofx.launch
@@ -10,6 +11,7 @@ class ChezzApp : App(ChezzAppView::class) {
         stage.isResizable = false
         stage.height = 800.0
         stage.width = 1000.0
+        stage.icons.add(Image("/chess_icon.png"))
     }
 
 }
