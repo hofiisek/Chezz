70d6cff3f6ba02f247f495e8d58680a384efbc81
DHf | Dominik Hoftych <dominik.hoftych@etnetera.cz>
2021-02-12 20:15:38 +0100

    Disabled undo feature when game hasn't been started yet

--- src/main/kotlin/ui/view/MenuView.kt
+++ src/main/kotlin/ui/view/MenuView.kt
@@ -2,6 +2,9 @@ package ui.view
 
 import javafx.application.Platform
 import tornadofx.*
+import ui.controller.BoardController
+import kotlin.properties.Delegates
+import kotlin.properties.ObservableProperty
 
 /**
  * Menu view
@@ -11,6 +14,7 @@ import tornadofx.*
 class MenuView : View() {
 
     private val view: BoardView by inject()
+    private val controller: BoardController by inject()
 
     override val root =  menubar {
         menu("Play") {
@@ -25,8 +29,10 @@ class MenuView : View() {
             item("Save game").action {
                 // TODO
             }
-            item("Undo last move").action {
-                // TODO
+            item("Undo last move") {
+                action {
+                    if (controller.isGameStarted()) view.undoLastMove()
+                }
             }
             item("Quit").action {
                 Platform.exit()
