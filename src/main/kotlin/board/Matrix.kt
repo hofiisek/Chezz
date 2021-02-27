178d30aa9e0df357cbbefaa1b88ec1b7a4c6617e
hofiisek <hofiisek@gmail.com>
2021-02-27 12:18:45 +0100

    Edited javadoc of Matrix

--- src/main/kotlin/board/Matrix.kt
+++ src/main/kotlin/board/Matrix.kt
@@ -1,33 +1,31 @@
 package board
 
 /**
- * Simple matrix implementation internally represented as a list of lists.
+ * Initializes a new matrix of [n] rows and [m] cols and initializes each element
+ * using the [initializer].
  *
- * Extends and delegates to the [List] interface but provides adjusted iterator
+ * Delegates to the [List] interface but provides adjusted iterator
  * so it can by easily iterated element by element just as it was a 1D array.
  *
- * Due to the custom iterator, the index provided by the well known [forEachIndexed(int, action)][List.forEachIndexed]
+ * Due to the custom iterator, the index provided by the [forEachIndexed(int, action)][List.forEachIndexed]
  * method behaves as it was a 1D array (thus ranging from 1 to rows*cols exclusive).
+ * To iterate through the matrix with both indices (row-wise and column-wise), use
+ * [forEachIndexed(row, col, action)][forEachIndexed] instead.
  *
- * To iterate through the matrix with both indices (row-wise and column-wise), please use
- * the [forEachIndexed(row, col, action)][forEachIndexed] method.
- *
- * @param rows number of rows
- * @param cols number of cols
+ * @param n number of rows
+ * @param m number of cols
  * @param initializer lambda function providing current row and column to initialize matrix elements
- *
- * TODO is this the right way? :((
  *
  * @author Dominik Hoftych
  */
 data class Matrix<T>(
-        val rows: Int,
-        val cols: Int,
-        private val initializer: Matrix<T>.(row: Int, col: Int) -> T
+    private val n: Int,
+    private val m: Int,
+    private val initializer: Matrix<T>.(row: Int, col: Int) -> T
 ) : List<T> by ArrayList() {
 
-    val matrix: List<List<T>> = List(rows) { row ->
-        List(cols) { col ->
+    val matrix: List<List<T>> = List(n) { row ->
+        List(m) { col ->
             initializer(row, col)
         }
     }
@@ -35,12 +33,33 @@ data class Matrix<T>(
     override operator fun iterator(): Iterator<T> = object : Iterator<T> {
         private var idx = 0
 
-        override fun hasNext(): Boolean = idx < rows * cols
+        override fun hasNext(): Boolean = idx < n * m
 
         override fun next(): T {
-            return matrix[idx/rows][idx++ % cols]
+            return matrix[idx/n][idx++ % m]
         }
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Matrix<*>
+
+        if (n != other.n) return false
+        if (m != other.m) return false
+        if (matrix != other.matrix) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = n
+        result = 31 * result + m
+        result = 31 * result + matrix.hashCode()
+        return result
+    }
+
 }
 
 
