c4007dc1e2f69cd6d303cfbc45699e61f7db33b2
DHf | Dominik Hoftych <dominik.hoftych@etnetera.cz>
2021-03-05 23:24:00 +0100

    Added implementation of algebraic notation including the resolutions of unambiguous moves

--- src/main/kotlin/io/AlgebraicNotations.kt
+++ src/main/kotlin/io/AlgebraicNotations.kt
@@ -1,13 +1,16 @@
 package io
 
+import board.Board
 import board.Position
-import board.Square
 import game.*
+import io.AmbiguityLevel.*
 import piece.*
 
 
 /**
- * The unicode character of the piece
+ * Unicode character representing the piece.
+ * Currently unused - will be used once importing/exporting of PGN
+ * files with unicode symbols of pieces is implemented.
  */
 val Piece.unicode: String
     get() = when(this) {
@@ -19,13 +22,12 @@ val Piece.unicode: String
         is King -> if (player == Player.WHITE) "\u2654" else "\u265A"
     }
 
-val Position.an: String
-    get() = "${file}${rank}"
+val Position.an: String get() = "${file}${rank}"
 
-val Square.an: String
-    get() = position.an
-
-val Piece?.an: String
+/**
+ * Letter representing the piece
+ */
+val Piece?.letter: String
     get() = when (this) {
         is Bishop -> "B"
         is King -> "K"
@@ -36,11 +38,125 @@ val Piece?.an: String
         null -> throw IllegalArgumentException()
     }
 
+/**
+ * Algebraic notation of the move
+ */
 val Move.an: String
     get() = when (this) {
-        // TODO add + sigh if the move is check move
-        is BasicMove -> "${piece.an}${if (isCapture && piece is Pawn) piece.position.file else ""}${if (isCapture) "x" else ""}${to.an}"
-        is PromotionMove -> "${basicMove.an}=${promotedTo.an}"
+        is BasicMove -> "${piece.letter}${if (isCapture && piece is Pawn) piece.position.file else ""}${if (isCapture) "x" else ""}${to.an}"
+        is PromotionMove -> "${basicMove.an}=${promotedTo.letter}"
         is CastlingMove -> "O-O${if (queenSide) "-0" else ""}"
         is EnPassantMove -> "${pawn.position.file}x${to.an}"
     }
+
+/**
+ * Algebraic notation of the move with the file of the origin position included
+ */
+val BasicMove.anWithFile: String
+    get() = when (piece) {
+        is Pawn -> this.an
+        else -> "${piece.letter}${piece.position.file}${this.an.drop(1)}"
+    }
+
+/**
+ * Algebraic notation of the move with the rank of the origin position included
+ */
+val BasicMove.anWithRank: String
+    get() = when (piece) {
+        is Pawn -> this.an
+        else -> "${piece.letter}${piece.position.rank}${this.an.drop(1)}"
+    }
+
+/**
+ * Algebraic notation of the move with the origin position included
+ */
+val BasicMove.anWithPosition: String
+    get() = when (piece) {
+        is Pawn -> this.an
+        else -> "${piece.letter}${piece.position.an}${this.an.drop(1)}"
+    }
+
+/**
+ * Returns the correct algebraic notation of this move considering
+ * the current [board], resolving all possible unambiguity if there
+ * are multiple pieces of the same type that can move to the same destination.
+ *
+ * Note that ambiguity can happen only for basic moves where the moving piece
+ * is not a pawn.
+ */
+fun Move.getAlgebraicNotation(board: Board): String = when {
+    this is BasicMove && this.piece !is Pawn -> {
+        // different pieces that can move to the same square
+        val otherPieces: List<Piece> = board.getPiecesFor(type = this.piece::class)
+            .filter { it != this.piece }
+            .flatMap { it.getAllowedMoves(board).filterIsInstance<BasicMove>() }
+            .filter { it.to == this.to }
+            .map { it.piece }
+
+        when (resolveAmbiguityLevel(this, otherPieces)) {
+            RANK_AND_FILE -> this.anWithPosition
+            RANK -> this.anWithFile
+            FILE -> this.anWithRank
+            DEFAULT -> this.anWithFile
+            else -> this.an
+        }
+    }
+    else -> this.an
+}
+
+/**
+ * Resolves the ambiguity level of the given [move] and the list of [pieces] that
+ * can move to the same destination
+ */
+private fun resolveAmbiguityLevel(move: BasicMove, pieces: List<Piece>): AmbiguityLevel {
+    fun Piece.sameRow(row: Int) = this.position.row == row
+    fun Piece.sameColumn(column: Int) = this.position.col == column
+
+    val (row, col) = move.piece.position
+    return when {
+        pieces.isEmpty() -> NONE
+        pieces.any { it.sameRow(row) } && pieces.any { it.sameColumn(col) } -> RANK_AND_FILE
+        pieces.any { it.sameRow(row) } -> RANK
+        pieces.any { it.sameColumn(col) } -> FILE
+        else -> DEFAULT
+    }
+}
+
+/**
+ * Ambiguity levels that need to be resolved to ensure that the
+ * algebraic notation of a move is unambiguous w.r.t. current board state.
+ */
+private enum class AmbiguityLevel {
+
+    /**
+     * There are multiple pieces of the same type on the same rank as well as
+     * on the same file, so both the rank and the file must be
+     * used to ensure unambiguity.
+     */
+    RANK_AND_FILE,
+
+    /**
+     * There are multiple pieces of the same type on the same rank,
+     * so the file must be used to ensure unambiguity
+     */
+    RANK,
+
+    /**
+     * There are multiple pieces of the same type on the same file,
+     * so the rank must be used to ensure unambiguity
+     */
+    FILE,
+
+    /**
+     * There are no other pieces on the same file nor on the same rank.
+     * In such situation, it's a convention to use the file to ensure
+     * unambiguity.
+     */
+    DEFAULT,
+
+    /**
+     * There are no pieces that can move to the same destination,
+     * standard algebraic notation can be used
+     */
+    NONE
+}
