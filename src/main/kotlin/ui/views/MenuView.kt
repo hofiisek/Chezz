c90326c63f15a4fd2249edc6037bbaf13d8e1b78
hofiisek <hofiisek@gmail.com>
2021-03-06 20:13:57 +0100

    Extended menu when saving games - form with tag pairs needs to be filled

--- src/main/kotlin/ui/views/MenuView.kt
+++ src/main/kotlin/ui/views/MenuView.kt
@@ -1,13 +1,22 @@
 package ui.views
 
+import game.GameResult
+import game.pgnString
 import javafx.application.Platform
+import javafx.beans.property.SimpleObjectProperty
+import javafx.beans.property.SimpleStringProperty
+import javafx.scene.Parent
 import javafx.stage.FileChooser.ExtensionFilter
+import javafx.stage.StageStyle
 import tornadofx.*
 import ui.controllers.BoardController
-import java.io.File
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
+import javax.swing.text.html.parser.TagElement
 
 /**
- * Menu view
+ * Navigation menu bar with shortcuts
  *
  * @author Dominik Hoftych
  */
@@ -26,7 +35,7 @@ class MenuView : View() {
 
             item("Load game", "Shortcut+L").action {
                 chooseFile(
-                    title = "Select PGN file to import",
+                    title = "Select a PGN file to import",
                     filters = arrayOf(ExtensionFilter(".pgn files", "*.pgn"))
                 ).firstOrNull()?.let { boardController.importPgn(it) }
             }
@@ -38,9 +47,12 @@ class MenuView : View() {
 
         menu("State") {
             item("Save game", "Shortcut+S").action {
-                // TODO show export options - PGN headers form
-                val pgn: String = boardController.exportPgn()
-                val result = chooseDirectory(title = "Select the directory to which to save the file") {  }
+                if (!boardController.hasGameStarted()) return@action
+
+                find<TagPairsFormView>(
+                    TagPairsFormView::pgn to boardController.exportPgn(),
+                    TagPairsFormView::result to boardController.getGameResult()
+                ).openModal(resizable = false)
             }
 
             item("Undo last move", "Shortcut+Z").action {
@@ -52,5 +64,65 @@ class MenuView : View() {
             // TODO - show check, show allowed moves, ..
         }
     }
+}
+
+class TagPairsFormView : Fragment("Tag pairs") {
+
+    val pgn: String by param()
+    val result: GameResult by param()
+
+    private val model = ViewModel()
+
+    // Seven Tag Roster
+    private val event = model.bind { SimpleStringProperty(null, "Event") }
+    private val site = model.bind { SimpleStringProperty(null, "Site")  }
+    private val date = model.bind { SimpleObjectProperty<LocalDate>(null, "Date")  }
+    private val round = model.bind { SimpleStringProperty(null, "Round")  }
+    private val white = model.bind { SimpleStringProperty(null, "White")  }
+    private val black = model.bind { SimpleStringProperty(null, "Black")  }
+
+    override val root: Parent = form {
+        fieldset {
+            field("Event") { textfield(event).required() }
+            field("Site") { textfield(site).required() }
+            field("Date") { datepicker(date).required() }
+            field("Round") { textfield(round).required() }
+            field("White") { textfield(white).required() }
+            field("Black") { textfield(black).required() }
+
+            // TODO center the button + sort properties
+            button("Done").enableWhen(model.valid).action {
+                chooseFile(
+                    title = "Select a PGN file to import",
+                    filters = arrayOf(ExtensionFilter(".pgn files", "*.pgn")),
+                    mode = FileChooserMode.Save
+                ).firstOrNull()?.let { file ->
+                    file.bufferedWriter().use {
+                        model.propertyMap.forEach { (t, _) ->
+                            it.write(t.value.asTagPair(t.name))
+                            it.newLine()
+                        }
+                        it.write(result.pgnString().asTagPair("Result"))
+
+
+                        it.newLine()
+                        it.newLine()
+                        it.write(pgn)
+                    }
+                    close()
+                }
+            }
+
+        }
+    }
+
+    private fun Any.asTagPair(tag: String): String {
+        val str = when (this) {
+            is LocalDate -> this.format(DateTimeFormatter.ofPattern("yyyy/MM/dd"))
+            else -> this
+        }
+        return """[$tag "$str"]"""
+    }
+
 
 }
