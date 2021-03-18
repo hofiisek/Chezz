cbf1d3e06486a792293ae37712298142e4dcfc8d
DHf | Dominik Hoftych <dominik.hoftych@etnetera.cz>
2021-03-18 22:38:41 +0100

    Using ktlint and detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,12 @@
+import io.gitlab.arturbosch.detekt.Detekt
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 
 plugins {
     kotlin("jvm") version "1.4.31"
+
+    id("org.jmailen.kotlinter") version "3.3.0"
+    id("io.gitlab.arturbosch.detekt") version "1.16.0"
 }
 
 group = "com.github.hofiisek"
@@ -9,26 +14,46 @@ version = "1.0"
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
-    implementation("no.tornado:tornadofx:1.7.20")
+    implementation(kotlin("reflect"))
     implementation(kotlin("stdlib-jdk8"))
+    implementation("no.tornado:tornadofx:1.7.20")
 
     val kotestVersion = "4.4.3"
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
     testImplementation("io.kotest:kotest-property:$kotestVersion")
 
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.4.31")
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.16.0")
+}
+
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    withType<Detekt> {
+        this.jvmTarget = "1.8"
+    }
 }
 
 tasks {
     test {
         useJUnitPlatform()
+
+        finalizedBy(lintKotlin, detekt)
     }
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+detekt {
+    baseline = file("config/detekt/detekt.yml")
+    parallel = true // Builds the AST in parallel. Rules are always executed in parallel.
+}
+
+tasks {
+    withType<Wrapper> {
+        gradleVersion = "6.6.1"
+    }
 }
